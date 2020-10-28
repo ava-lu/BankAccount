51ca5328a24d01242b6c32885fcd4fe5564a0ec1
ava-lu <alu21@stuy.edu>
2020-10-27 21:33:20 -0400

    Tested getAccountID

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester {
   public static void main(String[] args) {
-    BankAccount first = new BankAccount(5467, "password");
+    BankAccount first = new BankAccount(67, "password");
     System.out.println(first.getBalance());
+    System.out.println(first.getAccountID());
   }
 }
