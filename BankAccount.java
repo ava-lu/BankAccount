f201d81742ca731b5ed8074a59bd32a225614b51
ava-lu <alu21@stuy.edu>
2020-10-27 21:41:29 -0400

    Finished deposit() and tested case

--- BankAccount.java
+++ BankAccount.java
@@ -23,4 +23,12 @@ public class BankAccount{
     password = newPass;
   }
 
+  public boolean deposit(double amount) {
+    if (amount >= 0) {
+      balance+=amount;
+      return true;
+    }
+    else return false;
+  }
+
 }
